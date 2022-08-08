2e8d8cb9249c286e54ad18968a5e580a91306604
DtPhat <91726373+DtPhat@users.noreply.github.com>
2022-08-08 23:41:49 +0700

    A little tweak for responsive display

--- src/components/Home.js
+++ src/components/Home.js
@@ -74,7 +74,7 @@ export default function ({ setFormData }) {
     return (
         <div className="home">
             <h1 className="home-title"><span>Quiz</span>Generator</h1>
-            <p className="home-description">All of the used data is fetched from <a href="https://opentdb.com" target="_blank">https://opentdb.com/</a></p>
+            <p className="home-description">All of the used data is fetched from <a href="https://opentdb.com" target="_blank">https://opentdb.com</a></p>
             {formElement}
             <a href="https://github.com/DtPhat/Quiz_Generator" target="_blank"><img src="./images/blue-creep.jpg" alt="" className="info-avatar"></img></a>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -22,7 +22,8 @@ export default function (props) {
         : null
     return (
         <div>
-            <div className="quiz-info" data-hover={`Amount: ${formData.trivia_amount}
+            <div className="quiz-info" data-hover={`QUIZ INFORMATION:
+            Amount: ${formData.trivia_amount}
             Category: ${formData.trivia_category ? formData.trivia_category : "any category"} 
             Difficulty: ${formData.trivia_difficulty ? formData.trivia_difficulty : "any difficulty"} 
             Type: ${formData.trivia_type ? formData.trivia_type : "any type"}`}>?</div>
